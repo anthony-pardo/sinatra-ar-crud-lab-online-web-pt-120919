9b51e49b03d586aaa8203a8fc2bd2826f44e2cbf
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-24 21:03:02 +0000

    Automatically backed up by Learn

--- app/views/edit.rb
+++ app/views/edit.rb
@@ -1,4 +1,5 @@
 <form action="/articles/<%= @article.id %>" method="post">
     <input id="hidden" type="hidden" name="_method" value="patch">
     <input type="text" name="title">
+    <input type="text" name="content">
 </form>
